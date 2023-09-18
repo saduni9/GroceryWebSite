5f21978028d077773dd30245ad0aeffa54384f8c
saduni9 <rasikalahms.20@uom.lk>
2023-09-18 17:55:38 +0530

    first commits for the grocery website

---
+++ script.js
@@ -0,0 +1,48 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    shoppinCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
+
+let shoppinCart = document.querySelector('.shoppin-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppinCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
+
+let loginForm= document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    shoppinCart.classList.remove('active');
+   
+    
+}
+
+let navbar= document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppinCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    shoppinCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    
+}
